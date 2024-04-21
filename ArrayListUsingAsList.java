9c0a437cf644c0393105113598e01de2d0fc9d4f
Padmaksh Terkar <padmakshterkar96@gmail.com>
2024-04-21 20:44:09 +0530

    Arraylist Example

---
+++ ArrayListUsingAsList.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class ArrayListUsingAsList {
+    public static void main(String args[])
+    {
+        //Arraylist of string type
+        ArrayList<String> str=new ArrayList<String>(Arrays.asList("I","Love","Topperworld"));
+        //initilize an arraylist with add()
+        // str.add("Topper");
+        // str.add("is");
+        // str.add("padmaksh");
+
+        System.out.println("Arraylist is :"+ str);
+    }
+    
+}
